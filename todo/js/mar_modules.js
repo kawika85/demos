28e8e9348cdc483e3901587a436719fc16b113e3
David <kawika@kawika-Desk.fritz.box>
2014-02-24 11:32:24 +0100

    Added tasks views/models

--- todo/js/mar_modules.js
+++ todo/js/mar_modules.js
@@ -12,7 +12,8 @@ TodosApp.module('Todos.models', function (Todos, App, Backbone) {
 			        title: null,
 			        description: null,
 			        done:0,
-			        created:null
+			        created:null,
+                    tasks:null
                 },
                 validate: function (attrs) {
 
@@ -28,12 +29,39 @@ TodosApp.module('Todos.models', function (Todos, App, Backbone) {
 			        if (this.isNew()) {
 				        this.set('created', Date.now());
 			        }
-                    
+                    if(!this.get("tasks"))
+                        this.set("tasks",new Todos.TaskList())
+
 		        },
+                 sync: function(method, model, options) {
+
+                      if(model.get("tasks")){
+                        var task_str=JSON.stringify(_.map(model.get("tasks").models,function(e){return [e.get("description"),e.get("done")]}))
+                        model.set("tasks",task_str)
+                    }
+                      Backbone.sync(method, model, options);
+                },
+                parse : function(resp) {
+console.log("resp.tasks",resp.tasks)
+                    if(resp.tasks)
+                        resp.tasks=new Todos.TaskList(_.map(JSON.parse(resp.tasks),function(e){return {description:e[0],done:e[1]}}))
+
+                    return resp;
+                }
 
         });
 
+        Todos.TaskInst = Backbone.Model.extend({
+                defaults: {
+			        description: null,
+			        done:0
+                }
+        });
 
+        Todos.TaskList = Backbone.Collection.extend({
+                model: Todos.TaskInst
+				
+        })
         Todos.TodoList = Backbone.Collection.extend({
                 model: Todos.TodoInst,
                 localStorage: new Backbone.LocalStorage('TodosApp-todolist'),
@@ -54,8 +82,6 @@ TodosApp.module('Todos.views', function (TodoViews, App, Backbone, Marionette, $
             'click .cancel':'Cancel'
         },
         render:function (eventName) {
-            //rewrite render. use the same view for render collection or model popup
-            //will be solved when "recursive todos" are implemented (todo tree with parents)
             var title_=(this.model.models)?"the entire collection":this.model.get("title")
             var template_ = _.template($(this.template).html(), {modal_title: title_});
             $(this.el).html(template_);
@@ -78,14 +104,36 @@ TodosApp.module('Todos.views', function (TodoViews, App, Backbone, Marionette, $
         }     
      });
 
-	TodoViews.ItemView = Marionette.ItemView.extend({
-		tagName: 'tr',
+    TodoViews.NoItemsView = Backbone.Marionette.ItemView.extend({
+        template: "#template-noitems"
+    });
+	TodoViews.ItemView = Backbone.Marionette.Layout.extend({
+		tagName: 'div',
+        className: 'row',
 		template: '#template-todo-row',
 		events: {
 			'click .save:not(.disabled)': 'Save',
 			'click .remove': 'Remove',
 			'click .done': 'ToggleDone',
+			'click .show-tasks': 'ShowTasks',
+
 		},
+        ShowTasks:function(e){
+
+            if(this.$el.find(".tasks_container").length==0){
+                var este=this
+                this.$el.append($('<div />').attr({ class: "tasks_container col-xs-12"}))
+                this.addRegions({tasks:".tasks_container"})
+                var task_view=new TodoViews.TaskListView({collection:este.model.get("tasks")})
+                this.tasks.show(task_view)
+                this.$el.find(".save").removeClass("disabled")
+                $(e.currentTarget).removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up")
+            }else{
+                $(e.currentTarget).removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down")
+                this.tasks.close();
+                this.$el.find(".tasks_container").empty().remove()
+            }
+        },
 		modelEvents: {
 			'change': 'render'
 		},
@@ -102,7 +150,7 @@ TodosApp.module('Todos.views', function (TodoViews, App, Backbone, Marionette, $
         Save: function (e) {
             var target=(e)?$(e.currentTarget):this.$el.find(".save")
             this.model.save({},{
-                                success:function(){console.log("target",target)
+                                success:function(){
                                             target.removeClass("glyphicon-floppy-disk").addClass("glyphicon-floppy-saved").addClass("disabled")
                                         },
                                 error:function(){
@@ -118,7 +166,8 @@ TodosApp.module('Todos.views', function (TodoViews, App, Backbone, Marionette, $
 	TodoViews.ListView = Backbone.Marionette.CompositeView.extend({
 		template: '#template-todo-list',
 		itemView: TodoViews.ItemView,
-		itemViewContainer: 'tbody',
+		itemViewContainer: '.list-cont',
+        emptyView: TodoViews.NoItemsView,
 		events: {
 	        'click .toggle_all': 'ToggleAll',
 	        'click .save_all': 'SaveAll',
@@ -144,14 +193,68 @@ TodosApp.module('Todos.views', function (TodoViews, App, Backbone, Marionette, $
 
 	});
 
+	TodoViews.TaskView = Marionette.ItemView.extend({
+		tagName: 'div',
+        className: 'row',
+		template: '#template-task-row',
+		events: {
+			'click .remove': 'Remove',
+			'click .task-done': 'ToggleDone',
+			'click .edit': 'EnEdit',
+            'dblclick .editable': 'EnEdit',
+			'keyup .editable input': 'ChangeDesc',
+            'change .editable input': 'DisEdit',
+		},
+        DisEdit: function (e) {
 
-});
+
+            this.$el.find(".editable").addClass("disabled").removeClass("enabled")
+
+        },
+        EnEdit: function (e) {
+
+            if(this.$el.find(".editable").hasClass("disabled"))
+                this.$el.find(".editable").removeClass("disabled").addClass("enabled")
+            else
+                this.$el.find(".editable").addClass("disabled").removeClass("enabled")
+        
+            this.$el.find(".editable input").focus();
+        },
+        ChangeDesc: function (e) {
+            this.model.set("description",$(e.currentTarget).val())
+            this.$el.find(".editable>span").html($(e.currentTarget).val())
+        },
+        ToggleDone: function (e) {
+            this.model.set("done",(this.model.get("done")==1)?0:1)
+            if(this.model.get("done")==1)
+                this.$el.find(".task-done").addClass("glyphicon-check").removeClass("glyphicon-unchecked")
+            else
+                this.$el.find(".task-done").addClass("glyphicon-unchecked").removeClass("glyphicon-check")
+        },
+        Remove: function (e) {
+			this.model.destroy()
+        }
+    });
+
+	TodoViews.TaskListView = Backbone.Marionette.CompositeView.extend({
+		template: '#template-task-list',
+		itemView: TodoViews.TaskView,
+		itemViewContainer: '.task-cont',
+        emptyView: TodoViews.NoItemsView,
+		events: {
+			'click .add_task': 'addTask',
+		},
+        addTask: function (e) {
+            this.collection.add(new App.Todos.models.TaskInst())
+            var child_view=this.children.findByIndex(this.children.length-1)
+            child_view.$el.find(".editable").removeClass("disabled").addClass("enabled")
+            child_view.$el.find(".editable input").focus();
+        }
+    });
 
 
-//LAYOUT
-TodosApp.module('Layout', function (Layout, App, Backbone) {
 
-	Layout.Header = Backbone.Marionette.ItemView.extend({
+	TodoViews.Header = Backbone.Marionette.ItemView.extend({
 		template: '#template-todo-header',
 		events: {
 			'click .add_todo': 'addTodo'
@@ -161,19 +264,25 @@ TodosApp.module('Layout', function (Layout, App, Backbone) {
         }
 	})
 
-	Layout.Formu = Backbone.Marionette.ItemView.extend({
+
+	TodoViews.Formu = Backbone.Marionette.Layout.extend({
 		template: '#template-todo-form',
+        regions:{
+            tasksform: '.tasks-form'    
+        },
         events: {
 			'click .submit_form': 'submitForm'
 		},
-
+        onRender:function() {
+            var task_view=new TodoViews.TaskListView({collection:this.model.get("tasks")})
+            this.tasksform.show(task_view)
+        },
         submitForm: function (e) {
 
             var este=this
-            var done_=(this.$el.find(".done").is(":checked"))?1:0
-            this.model.set("title",this.$el.find(".title").val())
-            this.model.set("description",this.$el.find(".desc").val())
-            this.model.set("done",done_)
+            this.model.set({"title":this.$el.find(".title").val(),
+                            "description":this.$el.find(".desc").val(),
+                            "done":(this.$el.find(".done").is(":checked"))?1:0})
 
             this.$el.find("input,textarea").tooltip('destroy')
             if(this.model.isValid())
@@ -184,7 +293,9 @@ TodosApp.module('Layout', function (Layout, App, Backbone) {
                 }else
                     this.model.save()
 
-                this.$el.slideUp("fast",function(){este.close();})                
+                this.$el.slideUp("fast",function(){este.close();})    
+                window.location.hash=""
+
             }else
             {
                 _.each(this.model.validate(this.model.attributes),
@@ -198,7 +309,8 @@ TodosApp.module('Layout', function (Layout, App, Backbone) {
                                             });
 
             }
-            e.preventDefault();
+
+            e.preventDefault();   
 
 
         }
@@ -230,10 +342,10 @@ TodosApp.module('Todos', function (Todos, App, Backbone, Marionette, $, _) {
 
 		start: function () {
 
+            this.todoList.fetch();
 			this.showHeader(this.todoList);
 			this.showTodoList(this.todoList);
-			this.todoList.fetch();
-            var este=this
+			var este=this
             Todos.on("todo:add", function(){
                     este.showForm();
                 });
@@ -244,7 +356,7 @@ TodosApp.module('Todos', function (Todos, App, Backbone, Marionette, $, _) {
 		},
 
 		showHeader: function (todoList) {
-			var header = new App.Layout.Header({
+			var header = new Todos.views.Header({
 				collection: todoList
 			});
 			App.header.show(header);
@@ -256,7 +368,7 @@ TodosApp.module('Todos', function (Todos, App, Backbone, Marionette, $, _) {
         },
         Edit: function (id) {
             if(this.todoList.get(id)){
-			    var form_v = new App.Layout.Formu({
+			    var form_v = new Todos.views.Formu({
 				    model: this.todoList.get(id)
 			    });
 			    App.aux_form.show(form_v);
@@ -264,12 +376,14 @@ TodosApp.module('Todos', function (Todos, App, Backbone, Marionette, $, _) {
                 //not normal, bug?
                 App.aux_form.$el.show()
 
+
             }
+
         },
         showForm: function (todomodel) {
             var aux_ent=new App.Todos.models.TodoInst({inc_id:this.todoList.length+1})
             //this.todoList.create(aux_ent)
-			var form_ = new App.Layout.Formu({
+			var form_ = new Todos.views.Formu({
 				model: aux_ent,
                 collection:this.todoList
 			});
@@ -299,6 +413,7 @@ Todos.addInitializer(function () {
 		controller: controller
 	});
 	controller.start();
+
 });
 
 
--- todo/tests/spec/TodoSpec.js
+++ todo/tests/spec/TodoSpec.js
@@ -15,19 +15,38 @@ describe("Test Model", function() {
     it("shouldn't validate the task", function() {
          expect(this.nv_todo_.isValid()).toEqual(false)
     });
-    it("should add the task", function() {
+    it("should add/remove the task", function() {
         this.todo_list.create(this.v_todo_)
          expect(this.todo_list.length).toEqual(1)
-    });
-    it("should remove the task", function() {
-        this.todo_list.create(this.v_todo_)
-        this.v_todo_.destroy()
+         this.v_todo_.destroy()
          expect(this.todo_list.length).toEqual(0)
     });
 
 
 });
 
+describe("Test Views", function() {
+
+    beforeEach(function() {
+        this.todoModels = window.TodosApp.Todos.models
+    });
+
+    it("should render form", function() {
+            var aux_tasks=new this.todoModels.TaskList([{description:"task1",done:1},{description:"task2",done:0}])
+            var aux_ent=new this.todoModels.TodoInst({description:"bla bla bla",title:"title!",tasks:aux_tasks})
+
+			var form_ = new  window.TodosApp.Todos.views.Formu({	model: aux_ent });
+            form_.render()
+
+            expect( form_.$el.find('.title').val() ).toBe("title!")
+            expect( form_.$el.find('.desc').val() ).toBe("bla bla bla")
+            expect( form_.$el.find('.task-cont .row:eq( 0 ) span').first().text() ).toBe("task1")
+            expect( form_.$el.find('.task-cont .row:eq( 1 ) span').first().text() ).toBe("task2")
+
+    });
+
+
+});
 /*
 describe("Test Routes", function() {
 
